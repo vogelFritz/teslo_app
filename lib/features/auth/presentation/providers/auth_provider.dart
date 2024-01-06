e76dafb689e0603afe700931ff200daf14a07303
fritz <franciscosantin2002@gmail.com>
2024-01-06 11:43:32 -0300

    login y logout desde el provider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -12,11 +12,35 @@ class AuthNotifier extends StateNotifier<AuthState> {
   final AuthRepository authRepository;
   AuthNotifier({required this.authRepository}) : super(AuthState());
 
-  void loginUser(String email, String password) async {}
+  void loginUser(String email, String password) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    try {
+      final User user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredentials {
+      logout('Credenciales no son correctas');
+    } catch (e) {
+      logout('Error no controlado');
+    }
+  }
 
   void registerUser(String email, String password, String fullName) async {}
 
   void checkAuthStatus() async {}
+
+  void _setLoggedUser(User user) {
+    //TODO: Necesito guardar el token físicamente
+    state = state.copyWith(user: user, authStatus: AuthStatus.authenticated);
+  }
+
+  Future<void> logout([String? errorMessage]) async {
+    //TODO: limpiar token
+    state = state.copyWith(
+        user: null,
+        authStatus: AuthStatus.notAuthenticated,
+        errorMessage: errorMessage);
+  }
 }
 
 enum AuthStatus { checking, authenticated, notAuthenticated }
