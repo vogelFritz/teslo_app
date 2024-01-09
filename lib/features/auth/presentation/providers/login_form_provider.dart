298c4c7e47bebaec2bb5692dbcc6ffb55c88966c
fritz <franciscosantin2002@gmail.com>
2024-01-09 18:27:58 -0300

    bloquear boton ingresar

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -31,8 +31,9 @@ class LoginFormNotifier extends StateNotifier<LoginFormState> {
   onFormSubmit() async {
     _touchEveryField();
     if (!state.isValid) return;
-
+    state = state.copyWith(isPosting: true);
     await loginUserCallback(state.email.value, state.password.value);
+    state = state.copyWith(isPosting: false);
   }
 
   _touchEveryField() {
