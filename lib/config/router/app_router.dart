8927c712f8479f9e0c93b5ded6d5382a53e963d7
fritz <franciscosantin2002@gmail.com>
2024-01-09 18:10:53 -0300

    GoRouterNotifier

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,32 +1,42 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/config/router/app_router_notifier.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
-    ///*Primera pantalla
-    GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),
+final goRouterProvider = Provider((ref) {
+  final goRouterNotifier = ref.read(goRouterNotifierProvider);
+  return GoRouter(
+      initialLocation: '/login',
+      refreshListenable: goRouterNotifier,
+      routes: [
+        ///*Primera pantalla
+        GoRoute(
+          path: '/splash',
+          builder: (context, state) => const CheckAuthStatusScreen(),
+        ),
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+        ///* Auth Routes
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          path: '/register',
+          builder: (context, state) => const RegisterScreen(),
+        ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
+        ///* Product Routes
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const ProductsScreen(),
+        ),
+      ],
+      redirect: (context, state) {
+        print(state.subloc);
+        return null;
+      }
 
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+      ///! TODO: Bloquear si no se está autenticado de alguna manera
+      );
+});
