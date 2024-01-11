c26732138655adfb9796305e1c53ba57e6e9325e
fritz <franciscosantin2002@gmail.com>
2024-01-11 12:53:09 -0300

    productFormProvider

--- lib/features/products/presentation/providers/providers.dart
+++ lib/features/products/presentation/providers/providers.dart
@@ -1,3 +1,4 @@
+export 'forms/product_form_provider.dart';
+export 'product_provider.dart';
 export 'products_provider.dart';
 export 'products_repository_provider.dart';
-export 'product_provider.dart';
--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -26,13 +26,14 @@ class ProductScreen extends ConsumerWidget {
   }
 }
 
-class _ProductView extends StatelessWidget {
+class _ProductView extends ConsumerWidget {
   final Product product;
 
   const _ProductView({required this.product});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productForm = ref.watch(productFormProvider(product));
     final textStyles = Theme.of(context).textTheme;
 
     return ListView(
@@ -40,10 +41,11 @@ class _ProductView extends StatelessWidget {
         SizedBox(
           height: 250,
           width: 600,
-          child: _ImageGallery(images: product.images),
+          child: _ImageGallery(images: productForm.images),
         ),
         const SizedBox(height: 10),
-        Center(child: Text(product.title, style: textStyles.titleSmall)),
+        Center(
+            child: Text(productForm.title.value, style: textStyles.titleSmall)),
         const SizedBox(height: 10),
         _ProductInformation(product: product),
       ],
@@ -57,6 +59,7 @@ class _ProductInformation extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final productForm = ref.watch(productFormProvider(product));
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
@@ -67,42 +70,68 @@ class _ProductInformation extends ConsumerWidget {
           CustomProductField(
             isTopField: true,
             label: 'Nombre',
-            initialValue: product.title,
+            initialValue: productForm.title.value,
+            onChanged:
+                ref.read(productFormProvider(product).notifier).onTitleChanged,
+            errorMessage: productForm.title.errorMessage,
           ),
           CustomProductField(
-            label: 'Slug',
-            initialValue: product.slug,
-          ),
+              label: 'Slug',
+              initialValue: productForm.slug.value,
+              onChanged:
+                  ref.read(productFormProvider(product).notifier).onSlugChanged,
+              errorMessage: productForm.slug.errorMessage),
           CustomProductField(
-            isBottomField: true,
-            label: 'Precio',
-            keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            initialValue: product.price.toString(),
-          ),
+              isBottomField: true,
+              label: 'Precio',
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              initialValue: productForm.price.value.toString(),
+              onChanged: (value) => ref
+                  .read(productFormProvider(product).notifier)
+                  .onPriceChanged(double.tryParse(value) ?? -1),
+              errorMessage: productForm.price.errorMessage),
           const SizedBox(height: 15),
           const Text('Extras'),
-          _SizeSelector(selectedSizes: product.sizes),
+          _SizeSelector(
+              selectedSizes: productForm.sizes,
+              onSizesChanged: ref
+                  .read(productFormProvider(product).notifier)
+                  .onSizesChanged),
           const SizedBox(height: 5),
-          _GenderSelector(selectedGender: product.gender),
+          _GenderSelector(
+              selectedGender: productForm.gender,
+              onGenderChanged: ref
+                  .read(productFormProvider(product).notifier)
+                  .onGenderChanged),
           const SizedBox(height: 15),
           CustomProductField(
             isTopField: true,
             label: 'Existencias',
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            initialValue: product.stock.toString(),
+            initialValue: productForm.inStock.value.toString(),
+            onChanged: (value) => ref
+                .read(productFormProvider(product).notifier)
+                .onStockChanged(int.tryParse(value) ?? -1),
+            errorMessage: productForm.inStock.errorMessage,
           ),
           CustomProductField(
             maxLines: 6,
             label: 'Descripción',
             keyboardType: TextInputType.multiline,
-            initialValue: product.description,
+            initialValue: productForm.description,
+            onChanged: ref
+                .read(productFormProvider(product).notifier)
+                .onDescriptionChanged,
           ),
           CustomProductField(
             isBottomField: true,
             maxLines: 2,
             label: 'Tags (Separados por coma)',
             keyboardType: TextInputType.multiline,
-            initialValue: product.tags.join(', '),
+            initialValue: productForm.tags,
+            onChanged:
+                ref.read(productFormProvider(product).notifier).onTagsChanged,
           ),
           const SizedBox(height: 100),
         ],
@@ -114,8 +143,10 @@ class _ProductInformation extends ConsumerWidget {
 class _SizeSelector extends StatelessWidget {
   final List<String> selectedSizes;
   final List<String> sizes = const ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+  final void Function(List<String> selectedSizes) onSizesChanged;
 
-  const _SizeSelector({required this.selectedSizes});
+  const _SizeSelector(
+      {required this.selectedSizes, required this.onSizesChanged});
 
   @override
   Widget build(BuildContext context) {
@@ -129,7 +160,7 @@ class _SizeSelector extends StatelessWidget {
       }).toList(),
       selected: Set.from(selectedSizes),
       onSelectionChanged: (newSelection) {
-        print(newSelection);
+        onSizesChanged(List.from(newSelection));
       },
       multiSelectionEnabled: true,
     );
@@ -139,13 +170,15 @@ class _SizeSelector extends StatelessWidget {
 class _GenderSelector extends StatelessWidget {
   final String selectedGender;
   final List<String> genders = const ['men', 'women', 'kid'];
+  final void Function(String selectedGender) onGenderChanged;
   final List<IconData> genderIcons = const [
     Icons.man,
     Icons.woman,
     Icons.boy,
   ];
 
-  const _GenderSelector({required this.selectedGender});
+  const _GenderSelector(
+      {required this.selectedGender, required this.onGenderChanged});
 
   @override
   Widget build(BuildContext context) {
@@ -162,7 +195,7 @@ class _GenderSelector extends StatelessWidget {
         }).toList(),
         selected: {selectedGender},
         onSelectionChanged: (newSelection) {
-          print(newSelection);
+          onGenderChanged(newSelection.first);
         },
       ),
     );
